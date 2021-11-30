6935c040958c878982a460a5736fc02e9338ca9c
Jason Marshall <jason.marshall@temenos.com>
2021-11-30 11:08:23 -0700

    removed comments

--- Barn2b/Barn2b.ino
+++ Barn2b/Barn2b.ino
@@ -1,21 +1,3 @@
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
-  This example shows how to take Sensor Events instead of direct readings
-  
-  Designed specifically to work with the Adafruit BMP280 Breakout
-  ----> http://www.adafruit.com/products/2651
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
