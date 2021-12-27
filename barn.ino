b5958384091420a3f7029db442cd543eaaf2c12a
jasonmarshallc2 <44149100+jasonmarshallc2@users.noreply.github.com>
2021-12-27 11:45:34 -0700

    fixed display issue

--- barn.ino
+++ barn.ino
@@ -10,7 +10,9 @@ int powerSwitch = 4;
 unsigned long delayTime = millis();
 int setTemp = 0;
 bool systemPower = false;
-
+int currentTemp = 0;
+sensors_event_t temp_event, pressure_event;
+  
 Adafruit_BMP280 bmp; // use I2C interface
 Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
@@ -19,6 +21,10 @@ Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 // with the arduino pin number it is connected to
 //const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 const int rs = 7, en = 8, d4 = 9, d5 = 11, d6 = 12, d7 = 13;
+
+//const int rs =     7,  8, 9,11,12,13;
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2); example 
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
@@ -62,20 +68,20 @@ if (digitalRead(powerSwitch) == HIGH){
     systemPower = false;
     furniceOff();
     displayOff();
-    Serial.println("power off");
+    Serial.println("Power off");
   } else {
     systemPower = true;
     displayOn();
     Serial.println("Power on");
   }
 }
-//Serial.print(" systemPower: ");
-//Serial.println(systemPower);
+
 if (systemPower){
-  sensors_event_t temp_event, pressure_event;
+//  sensors_event_t temp_event, pressure_event;
   bmp_temp->getEvent(&temp_event);
-  bmp_pressure->getEvent(&pressure_event);
-  double currentTemp = (temp_event.temperature) * 9/5 + 32;
+//  bmp_pressure->getEvent(&pressure_event);
+//  double currentTemp = (temp_event.temperature) * 9/5 + 32;
+  currentTemp = (temp_event.temperature) * 9/5 + 32;
 //  Serial.print(F("Temperature = "));
 //  Serial.print(currentTemp);
 //  Serial.println(" *F");
@@ -83,34 +89,37 @@ if (systemPower){
 //  Serial.print(F("Pressure = "));
 //  Serial.print(pressure_event.pressure);
 //  Serial.println(" hPa");
-  Serial.println();
+//  Serial.println();
 
-  lcd.clear();
-  lcd.setCursor(1, 0);
-  lcd.print("Temp ");
-  lcd.print(currentTemp);
+  printCurrentTemp();
+  Serial.print("Humidity = ");
+  Serial.print(bmp.readHumidity());
+  Serial.println(" %");
 
   int temp = analogRead(setTempPin);
-  setTemp = (temp * 0.0488) + 40; // get about 45 degree total range 50/1024(pot range)
+//  setTemp = (temp * 0.0488) + 40; // get about 45 degree total range 50/1024(pot range)
+  setTemp = (temp * 0.0588) + 40; // get about 45 degree total range 60/1024(pot range)
 //  Serial.print("setTemp: ");
 //  Serial.println(setTemp);
-  lcd.setCursor(0, 1);
-  lcd.print("Set Temp ");
+  lcd.setCursor(1, 1);
+  lcd.print("Set  ");
   lcd.print(setTemp);
   
-  if ((setTemp - currentTemp) > 2){
+  if ((setTemp - currentTemp) > 0){
     furniceOn();
-  } else {
+  } else if ((setTemp - currentTemp) < -2) {
     furniceOff();
   }
   
-  delay(250);
+  delay(50);
 }
 }
 
 void furniceOn(){
   digitalWrite(furniceRelay, HIGH);
   Serial.println("Furnice ON");
+//  lcd.clear();
+  printCurrentTemp();
   lcd.setCursor(14, 0);
   lcd.print("ON");
 }
@@ -123,7 +132,18 @@ void furniceOff(){
 void displayOff(){
   lcd.clear();
   digitalWrite(lightPin, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
 }
 void displayOn(){
   digitalWrite(lightPin, LOW);
+}
+void printCurrentTemp(){
+  bmp_temp->getEvent(&temp_event);
+  currentTemp = (temp_event.temperature) * 9/5 + 32;
+  lcd.setCursor(1, 0);
+  lcd.print("Temp ");
+  lcd.print(currentTemp);
 }
